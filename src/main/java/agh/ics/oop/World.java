dbf6664654ca14bc7ebf1d711623e2c26978100d
Krzysztof Śliwiński <skrzysiek16.9@gmail.com>
2023-10-09 19:23:37 +0200

    Printing specific message based on provided argument

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -8,11 +8,23 @@ public class World {
     }
     public static void run(String args[]){
         System.out.println("Zwierzak idzie do przodu");
-        int index = 0;
+//        int index = 0;
+//        for(String arg: args){
+//            if(index < args.length - 1) System.out.print(arg + ", ");
+//            else System.out.println(arg);
+//            index++;
+//        }
         for(String arg: args){
-            if(index < args.length - 1) System.out.print(arg + ", ");
-            else System.out.println(arg);
-            index++;
+            String direction_text = switch(arg){
+                case "f" -> "do przodu";
+                case "b" -> "do tyłu";
+                case "l" -> "w lewo";
+                case "r" -> "w prawo";
+                default -> "";
+            };
+
+            if(direction_text != "") System.out.println("Zwierzak idzie " + direction_text);
+
         }
     }
 }
