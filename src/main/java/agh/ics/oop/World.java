20ae3b613cc96ed284ea93e77030d59894a6b743
Krzysztof Śliwiński <skrzysiek16.9@gmail.com>
2023-10-09 19:14:42 +0200

    Created run method printing basic message

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,6 +3,10 @@ package agh.ics.oop;
 public class World {
     public static void main(String args[]){
         System.out.println("Start");
+        run(args);
         System.out.println("Stop");
+    }
+    public static void run(String args[]){
+        System.out.println("Zwierzak idzie do przodu");
     }
 }
