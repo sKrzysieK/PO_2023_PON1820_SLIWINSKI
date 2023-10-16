41a0d35f7b419d54b7f3dc722735e965cc400f61
krzysiek <skrzysiek16.9@gmail.com>
2023-10-16 23:08:49 +0200

    Created MapDirection enum

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,6 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MapDirection;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
 
@@ -14,6 +15,17 @@ public class World {
         Vector2d position2 = new Vector2d(-2,1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
+
+        MapDirection direction = MapDirection.SOUTH;
+        System.out.println("This direction: " + direction.toString());
+        MapDirection nextDirection = direction.next();
+        MapDirection prevDirection = direction.previous();
+        System.out.println("Next direction: " + nextDirection);
+        System.out.println("Previous direction: " + prevDirection);
+        System.out.println("To Unit Vector: ");
+        System.out.println("This direction: " + direction.toUnitVector().toString());
+        System.out.println("Next direction: " + nextDirection.toUnitVector().toString());
+        System.out.println("Previous direction: " + prevDirection.toUnitVector().toString());
     }
     public static void run(MoveDirection directions[]){
         for(MoveDirection direction: directions){
