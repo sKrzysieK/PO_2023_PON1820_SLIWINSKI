0df896815ad583edb0d1e28daf03a9cb377c88b2
krzysiek <skrzysiek16.9@gmail.com>
2023-10-16 22:46:42 +0200

    Created Vector2d

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,21 +1,21 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
     public static void main(String args[]){
         System.out.println("Start");
         run(OptionsParser.parse(args));
         System.out.println("Stop");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
     public static void run(MoveDirection directions[]){
-//        System.out.println("Zwierzak idzie do przodu");
-//        int index = 0;
-//        for(String arg: args){
-//            if(index < args.length - 1) System.out.print(arg + ", ");
-//            else System.out.println(arg);
-//            index++;
-//        }
         for(MoveDirection direction: directions){
             String direction_text = switch(direction){
                 case FORWARD -> "do przodu";
---
+++ src/main/java/agh/ics/oop/model/Vector2d.java
@@ -0,0 +1,72 @@
+package agh.ics.oop.model;
+
+import java.util.Objects;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+
+    public Vector2d(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public String toString(){
+        return "(%s, %s)".formatted(x, y);
+    }
+
+    public boolean precedes(Vector2d other){
+        return other.x <= x && other.y <= y;
+    }
+
+    public boolean follows(Vector2d other){
+        return other.x >= x && other.y >= y;
+    }
+
+    public Vector2d add(Vector2d other){
+        return new Vector2d(x + other.x, y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other){
+        return new Vector2d(x - other.x, y - other.y);
+    }
+
+    public Vector2d upperRight(Vector2d other){
+        return new Vector2d(
+                x > other.x ? x : other.x,
+                y > other.y ? y : other.y
+        );
+    }
+
+    public Vector2d lowerLeft(Vector2d other){
+        return new Vector2d(
+                x < other.x ? x : other.x,
+                y < other.y ? y : other.y
+        );
+    }
+
+    public Vector2d opposite(){
+        return new Vector2d(-this.x, -this.y);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector2d)) return false;
+        Vector2d that = (Vector2d) o;
+        return x == that.x && y == that.y;
+    }
+
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+
+}
