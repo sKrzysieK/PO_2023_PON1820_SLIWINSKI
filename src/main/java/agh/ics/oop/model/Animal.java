2994e3fd3e0b124261530fb02ff07b127e86b3ec
krzysiek <skrzysiek16.9@gmail.com>
2023-10-26 21:38:03 +0200

    Fixed Animal move

--- src/main/java/agh/ics/oop/model/Animal.java
+++ src/main/java/agh/ics/oop/model/Animal.java
@@ -1,8 +1,10 @@
 package agh.ics.oop.model;
 
 public class Animal {
-    private MapDirection orientation = MapDirection.NORTH;
+    private final static int MAP_SIZE = 4;
+    private MapDirection orientation;
     private Vector2d position;
+
 
     public Animal(){
         this.position = new Vector2d(2,2);
@@ -30,6 +32,10 @@ public class Animal {
     }
 
     public void move(MoveDirection direction){
+        MapDirection startOrientation = this.orientation;
+        Vector2d mapStartPoint = new Vector2d(0,0);
+        Vector2d mapEndPoint = new Vector2d(MAP_SIZE, MAP_SIZE);
+
         // change orientation
         switch(direction){
             case BACKWARD -> this.orientation = this.orientation.next().next();
@@ -38,24 +44,19 @@ public class Animal {
         }
 
         // move
-        switch(this.orientation){
-            case NORTH -> {
-                Vector2d nextPosition = this.position.add(new Vector2d(0, 1));
-                if(nextPosition.getY() <= 4) this.position = nextPosition;
-            }
-            case SOUTH -> {
-                Vector2d nextPosition = this.position.add(new Vector2d(0, -1));
-                if(nextPosition.getY() >= 0) this.position = nextPosition;
-            }
-            case EAST -> {
-                Vector2d nextPosition = this.position.add(new Vector2d(1, 0));
-                if(nextPosition.getX() <= 4) this.position = nextPosition;
-            }
-            case WEST -> {
-                Vector2d nextPosition = this.position.add(new Vector2d(-1, 0));
-                if(nextPosition.getY() >= 0) this.position = nextPosition;
-            }
+        Vector2d nextPosition = switch(this.orientation){
+            case EAST -> this.position.add(new Vector2d(1,0));
+            case WEST -> this.position.add(new Vector2d(-1,0));
+            case NORTH -> this.position.add(new Vector2d(0,1));
+            case SOUTH -> this.position.add(new Vector2d(0,-1));
+        };
+
+        if(!(nextPosition.follows(mapStartPoint) && nextPosition.precedes(mapEndPoint))){
+            this.orientation = startOrientation;
+            return;
         }
+
+        this.position = nextPosition;
 
     }
 }
