930fb4572dc13e563dec54363b71424ba1aaa535
Krzysztof Śliwiński <skrzysiek16.9@gmail.com>
2023-11-25 15:07:42 +0100

    Modified WorldMap interface and World class

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,20 +3,44 @@ package agh.ics.oop;
 import agh.ics.oop.model.*;
 import agh.ics.oop.model.maps.GrassField;
 import agh.ics.oop.model.maps.RectangularMap;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class World {
-//    private final static RectangularMap map = new RectangularMap(4, 4);
-    private final static GrassField map = new GrassField(20);
+//    private final static RectangularMap rectMap = new RectangularMap(4, 4);
+//    private final static GrassField grassMap = new GrassField(20);
 
     public static void main(String[] args){
         try{
             List<MoveDirection> directions = OptionsParser.parse(args);
-            List<Vector2d> positions = List.of(new Vector2d(2,2),new Vector2d(2,3));
-            map.addListener(new ConsoleMapDisplay());
-            Simulation simulation = new Simulation(directions, positions, map);
-            simulation.run();
+            List<Vector2d> positions = List.of(new Vector2d(2,2));
+
+//            grassMap.addListener(new ConsoleMapDisplay());
+//            rectMap.addListener(new ConsoleMapDisplay());
+
+//            Simulation simulation1 = new Simulation(directions, positions, rectMap);
+//            Simulation simulation2 = new Simulation(directions, positions, grassMap);
+//            List<Simulation> simulations = List.of(simulation1, simulation2);
+
+            List<Simulation> simulations = new ArrayList<>();
+            for(int i = 0; i< 1000;i++){
+                RectangularMap rectMap = new RectangularMap(4, 4);
+                rectMap.addListener(new ConsoleMapDisplay());
+                simulations.add(new Simulation(directions, positions, rectMap));
+            }
+
+            SimulationEngine simulationEngine = new SimulationEngine(simulations);
+
+            simulationEngine.runAsync();
+
+            simulationEngine.awaitSimulationsEnd();
+
+            System.out.println("System zakończył działanie.");
+
         } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e){
             System.out.println(e.getMessage());
         }
     }
