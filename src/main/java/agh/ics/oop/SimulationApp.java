433c42da86e8d8635471d7d169177806b7b856b3
Krzysztof Śliwiński <skrzysiek16.9@gmail.com>
2023-12-08 21:50:25 +0100

    inital javafx setup

---
+++ src/main/java/agh/ics/oop/SimulationApp.java
@@ -0,0 +1,31 @@
+package agh.ics.oop;
+
+import agh.ics.oop.presenter.SimulationPresenter;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class SimulationApp extends Application {
+    public void start(Stage primaryStage){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+            BorderPane viewRoot = loader.load();
+            SimulationPresenter presenter = loader.getController();
+
+            configureStage(primaryStage, viewRoot);
+            primaryStage.show();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    private void configureStage(Stage primaryStage, BorderPane viewRoot) {
+        var scene = new Scene(viewRoot);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Simulation app");
+        primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
+        primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
+    }
+}
